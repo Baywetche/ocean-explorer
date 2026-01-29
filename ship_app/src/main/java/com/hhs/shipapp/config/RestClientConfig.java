57b7b03056303ea4fd072edc36b767c003799cd3
Abula Yalikun <abula.yalikun@vivavis.com>
2026-01-29 11:57:12 +0100

    RestClient Bean for shipBaseServer

---
+++ ship_app/src/main/java/com/hhs/shipapp/config/RestClientConfig.java
@@ -0,0 +1,17 @@
+package com.hhs.shipapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+  @Bean
+  public RestClient shipBaseRestClient() {
+    return RestClient.builder()
+        .baseUrl("http://localhost:8090/shipBaseServerAPI")
+        .build();
+  }
+
+}
